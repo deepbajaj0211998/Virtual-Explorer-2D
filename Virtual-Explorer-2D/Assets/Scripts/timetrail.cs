92969fd3b176ffe08a6e59ecbd40167dbd7c0349
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2023-01-14 10:48:57 +0530

    up-to-date project files

---
+++ Virtual-Explorer-2D/Assets/Scripts/timetrail.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class timetrail : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public GameObject gameoverscr;
+    public GameObject player;
+    public float totaltime=60f;
+    float time;
+    public float timeleft;
+    bool gameover=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reStart();
+    }
+    public void reStart()
+    {
+        time=0;
+        gameover=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameover==false)
+        {
+            time+=Time.deltaTime;
+            timeleft=totaltime-time;
+            int min =Mathf.FloorToInt(timeleft/60);
+            int sec =Mathf.FloorToInt(timeleft-min*60);
+            timeText.text=string.Format("{0:0}:{1:00}",min,sec);
+            if(timeleft==0)
+            {
+                gameover=true;
+                gameoverscr.transform.gameObject.SetActive(true);
+                player.transform.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void win()
+    {
+        gameover=true;
+
+    }
+    
+}
