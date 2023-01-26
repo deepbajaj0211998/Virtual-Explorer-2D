35f31303ee09b618d5da58e3cf2b76411dbe95f8
Atul272001 <atulkumar02072001@gmail.com>
2023-01-26 05:38:52 -0800

    AtulGodMode

--- Virtual-Explorer-2D/Assets/Scripts/uniqueFeatures/spikes.cs
+++ Virtual-Explorer-2D/Assets/Scripts/uniqueFeatures/spikes.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class spikes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
