839cc46ab232abdf54c14de143a5c98f9a42996b
Atul272001 <atulkumar02072001@gmail.com>
2023-01-30 08:17:53 -0800

    atul

--- Virtual-Explorer-2D/Assets/Scripts/Pickup_Scripts/InVincibility.cs
+++ Virtual-Explorer-2D/Assets/Scripts/Pickup_Scripts/InVincibility.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InVincibility : MonoBehaviour
 {
 
-    //public PrototypeHeroDemo prototypeHero;
-    public PlayerController2D player;
+    public PrototypeHeroDemo prototypeHero;
     BoxCollider2D boxCollider2d;
     SpriteRenderer spriteRenderer;
     public float invincibiltyTime;
+
+    public Slider slider;
 
 
     private void Start()
     {
         boxCollider2d = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        slider.gameObject.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D c2d)
@@ -32,9 +35,17 @@ public class InVincibility : MonoBehaviour
 
     IEnumerator Invinsible()
     {
-        player.godMode = true;
-        yield return new WaitForSeconds(invincibiltyTime);
-        player.godMode = false;
+        slider.gameObject.SetActive(true);
+        prototypeHero.godMode = true;
+        slider.maxValue = invincibiltyTime;
+        slider.value = invincibiltyTime;
+        for (int i = Mathf.FloorToInt(invincibiltyTime); i >= 0; i--)
+        {
+            yield return new WaitForSeconds(1f);
+            slider.value = i;
+        }
+        slider.gameObject.SetActive(false);
+        prototypeHero.godMode = false;
         Destroy(gameObject);
     }
 }
