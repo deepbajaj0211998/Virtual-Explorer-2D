82fdd9124bf4947a6af71dec7d41d7ef0edea027
monu shilla <smartmsrock@gmail.com>
2023-02-01 12:15:59 +0530

    monu
    upgraded enemy 1

--- Virtual-Explorer-2D/Assets/monu/scripts/kill_enemy.cs
+++ Virtual-Explorer-2D/Assets/monu/scripts/kill_enemy.cs
@@ -7,7 +7,7 @@ public class kill_enemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("kill"))
-            transform.parent.transform.GetComponent<AI>().TakeDamage(100);
+            transform.parent.transform.GetComponent<enemy1>().TakeDamage(100);
         
     }
 }
