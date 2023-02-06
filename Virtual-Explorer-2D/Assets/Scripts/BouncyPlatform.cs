e11ca2e1888a525e97c6a09477ac86e79708cf2f
Atul272001 <atulkumar02072001@gmail.com>
2023-02-06 07:15:47 -0800

    atul

---
+++ Virtual-Explorer-2D/Assets/Scripts/BouncyPlatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPlatform : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    public float minimumJumpForces;
+    public float maximumJumpForces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("BouncyPlatform"))
+        {
+            rigidbody2D.velocity = new Vector3(rigidbody2D.velocity.x, Random.Range(minimumJumpForces, maximumJumpForces));
+        }
+    }
+
+}
